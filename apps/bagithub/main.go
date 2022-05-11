238e0a6be413982646b4ffe7e4c0fcd5bc8ea796
llbec <30335141+bicliu@users.noreply.github.com>
2022-05-11 15:38:01 +0800

    update ba github

--- apps/bagithub/main.go
+++ apps/bagithub/main.go
@@ -13,16 +13,35 @@ var (
 	fFile string
 	fWeb  string
 	fName string
+	fCalc bool
 )
 
 func init() {
 	flag.StringVar(&fFile, "f", "", "specify file dir")
 	flag.StringVar(&fWeb, "w", "", "specify website url")
 	flag.StringVar(&fName, "n", "", "specify orgnazition name")
+	flag.BoolVar(&fCalc, "c", false, "calc list add or sub")
 }
 
 func main() {
 	flag.Parse()
+	if fCalc {
+		var urls []string
+		for {
+			a := ""
+			t := ""
+			v := ""
+			fmt.Printf("Enter add or sub, others quit\n")
+			fmt.Scanln(&a)
+			if a != "add" && a != "sub" {
+				break
+			}
+			fmt.Printf("Enter type(org,web,file) and value path(%v)\n", t)
+			fmt.Scanln(&t, &v)
+			urls = handleCalc(urls, a, t, v)
+		}
+		outputList(urls)
+	}
 	if fWeb != "" {
 		fmt.Println(fWeb, "start!")
 		urls, err := webList(fWeb)
@@ -53,6 +72,52 @@ func main() {
 	flag.Usage()
 }
 
+func handleCalc(src []string, act, v_type, v string) (urls []string) {
+	var tmp []string
+	var err error
+	switch v_type {
+	case "web":
+		tmp, err = webList(v)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	case "file":
+		tmp, err = fileList(v)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	default:
+		tmp, err = electricList(v)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+
+	srcmap := make(map[string]bool)
+	for _, v := range src {
+		v = formatUrl(v)
+		srcmap[v] = true
+	}
+	for _, v := range tmp {
+		v = formatUrl(v)
+		if act == "add" {
+			srcmap[v] = true
+		} else {
+			srcmap[v] = false
+		}
+	}
+	for k, v := range srcmap {
+		if v {
+			urls = append(urls, k)
+		}
+	}
+	fmt.Printf("src's length is %d, dest is %d\n", len(src), len(urls))
+	return
+}
+
 func webList(url string) (urls []string, err error) {
 	resp, err := website.GetWeb(url)
 	if err != nil {
@@ -68,7 +133,7 @@ func webList(url string) (urls []string, err error) {
 }
 
 func fileList(fileDir string) (urls []string, err error) {
-	doc, err := ioutil.ReadFile(fFile)
+	doc, err := ioutil.ReadFile(fileDir)
 	if err != nil {
 		return
 	}
@@ -94,8 +159,18 @@ func electricList(name string) (urls []string, err error) {
 }
 
 func outputList(list []string) {
-	for i, itm := range list {
-		fmt.Printf("%4d,\t%s\n", i, itm)
+	fmt.Printf("Total %d\n", len(list))
+	for _, itm := range list {
+		//fmt.Printf("%4d,\t%s\n", i, itm)
+		fmt.Printf("%s\n", itm)
 	}
 	os.Exit(0)
+}
+
+func formatUrl(str string) string {
+	l := len(str)
+	if str[l-1:l] == "/" {
+		str = str[:l-1]
+	}
+	return str
 }
