b043140e8d2492d1ce99a51ce1e03637d549e043
llbec <klins@163.com>
2023-05-25 20:15:26 +0800

    update adi

--- apps/adi/adi_test.go
+++ apps/adi/adi_test.go
@@ -7,6 +7,14 @@ import (
 )
 
 func TestHexCC(t *testing.T) {
-	cc := hexCC("1000100018F09FE5FFFFFFFF18F09FE518F09FE5")
+	cc := hexCC("1000100018F09FE50000A0E118F09FE518F09FE5BB")
 	fmt.Printf("%s\n", strconv.FormatInt(int64(cc), 16))
+}
+
+func TestRow(t *testing.T) {
+	row := NewHexRow("1000100018F09FE50000A0E118F09FE518F09FE5BB")
+	row.I2CEnable()
+	fmt.Printf("%v\n", row.Hex())
+	row.I2CDisable()
+	fmt.Printf("%v\n", row.Hex())
 }
--- apps/adi/hexfile.go
+++ apps/adi/hexfile.go
@@ -3,6 +3,7 @@ package adi
 import (
 	"fmt"
 	"strconv"
+	"strings"
 )
 
 /*
@@ -10,6 +11,99 @@ LL MM MM TT DD...DD CC
 length memory_offset type data CC
 cc = 100 - (LL + MM + MM + TT + DD + ... + DD)%100
 */
+type HexRow struct {
+	len    int
+	offset int
+	style  int
+	data   []byte
+	cc     int
+}
+
+func NewHexRow(hex string) *HexRow {
+	cc := hexCC(hex)
+
+	idx := 0
+	ll, e := strconv.ParseInt(string([]byte(hex)[idx:idx+2]), 16, 32)
+	if e != nil {
+		panic(e)
+	}
+	idx += 2
+
+	mm, e := strconv.ParseInt(string([]byte(hex)[idx:idx+4]), 16, 32)
+	if e != nil {
+		panic(e)
+	}
+	idx += 4
+
+	tt, e := strconv.ParseInt(string([]byte(hex)[idx:idx+2]), 16, 32)
+	if e != nil {
+		panic(e)
+	}
+	idx += 2
+
+	var buf []byte
+	for i := 0; i < int(ll); i++ {
+		dd, e := strconv.ParseInt(string([]byte(hex)[idx:idx+2]), 16, 32)
+		if e != nil {
+			panic(e)
+		}
+		idx += 2
+		buf = append(buf, byte(dd))
+	}
+
+	c, e := strconv.ParseInt(string([]byte(hex)[idx:idx+2]), 16, 32)
+	if e != nil {
+		panic(e)
+	}
+	if int(c) != cc {
+		panic(fmt.Errorf("check code invalid %v != %v", c, cc))
+	}
+
+	return &HexRow{
+		len:    int(ll),
+		offset: int(mm),
+		style:  int(tt),
+		data:   buf,
+		cc:     int(c),
+	}
+}
+
+func (row *HexRow) Hex() string {
+	//hex := strconv.FormatInt(int64(row.len), 16)
+	hex := fmt.Sprintf("%02x", row.len)
+	hex += fmt.Sprintf("%04x", row.offset)
+	hex += fmt.Sprintf("%02x", row.style)
+	for i := 0; i < row.len; i++ {
+		//hex += strconv.FormatInt(int64(row.data[i]), 16)
+		hex += fmt.Sprintf("%02x", row.data[i])
+	}
+	hex += fmt.Sprintf("%02x", row.cc)
+	return strings.ToUpper(hex)
+}
+
+func (row *HexRow) UpdateData(buf []byte, m int, l int) {
+	if l > len(buf) || m+l > row.len {
+		panic(fmt.Errorf("invalid para!"))
+	}
+	for i := 0; i < l; i++ {
+		row.data[m+i] = buf[i]
+	}
+	row.cc = hexCC(row.Hex())
+}
+
+func (row *HexRow) I2CEnable() {
+	if row.len != 0x10 || row.offset != 0x10 || row.style != 0 {
+		panic(fmt.Errorf("invalid string"))
+	}
+	row.UpdateData([]byte{0xFF, 0xFF, 0xFF, 0xFF}, 4, 4)
+}
+
+func (row *HexRow) I2CDisable() {
+	if row.len != 0x10 || row.offset != 0x10 || row.style != 0 {
+		panic(fmt.Errorf("invalid string"))
+	}
+	row.UpdateData([]byte{0x12, 0x34, 0x56, 0x78}, 4, 4)
+}
 
 /* str = ll mm mm tt dd...dd */
 func hexCC(str string) (cc int) {
@@ -18,8 +112,8 @@ func hexCC(str string) (cc int) {
 		panic(e)
 	}
 	lowLen := int(ll + 4)
-	if lowLen*2 != len(str) {
-		panic(fmt.Errorf("Invalid %d != %d", lowLen*2, len(str)))
+	if lowLen*2 > len(str) {
+		panic(fmt.Errorf("Invalid(%v) %d > %d", str, lowLen*2, len(str)))
 	}
 	sum := 0
 	for i := 0; i < lowLen; i++ {
