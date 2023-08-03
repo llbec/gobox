f48e92d6d630faa1f1cd39bbfca35eaf006c9383
llbec <klins@163.com>
2023-08-03 18:27:25 +0800

    update for curve

--- src/curve/curve.go
+++ src/curve/curve.go
@@ -20,35 +20,50 @@ func NewCurve(pow int) *Curve {
 	}
 }
 
-func (c *Curve) AddSample(x, y float64) {
-	c.samples = append(c.samples, []float64{x, y})
-	sort.Slice(c.samples, func(i, j int) bool { return c.samples[i][0] < c.samples[j][0] })
+func (crv *Curve) AddSample(x, y float64) {
+	crv.samples = append(crv.samples, []float64{x, y})
+	sort.Slice(crv.samples, func(i, j int) bool { return crv.samples[i][0] < crv.samples[j][0] })
 }
 
-func (c *Curve) Fitting() {
-	m := len(c.samples)
+func (crv *Curve) Fitting() error {
+	m := len(crv.samples)
 	Xv := []float64{}
 	Y := []float64{}
 
+	var matTheta, mat1, mat2 mat.Dense
+
 	for i := 0; i < m; i++ {
-		for j := 0; j < c.pow+1; j++ {
-			Xv = append(Xv, math.Pow(c.samples[i][0], float64(j)))
+		for j := 0; j < crv.pow+1; j++ {
+			Xv = append(Xv, math.Pow(crv.samples[i][0], float64(j)))
 		}
-		Y = append(Y, c.samples[i][1])
+		Y = append(Y, crv.samples[i][1])
 	}
-	matXv := mat.NewDense(m, c.pow+1, Xv)
+	matXv := mat.NewDense(m, crv.pow+1, Xv)
 	matY := mat.NewDense(m, 1, Y)
-	var matTheta mat.Dense
-	matTheta.Mul(matXv.T(), matXv)
-	err := matTheta.Inverse(&matTheta)
-	if err != nil {
-		fmt.Println(err)
-		return
+
+	mat1.Mul(matXv.T(), matXv)
+	if err := mat1.Inverse(&mat1); err != nil {
+		return err
 	}
-	matTheta.Mul(&matTheta, matXv.T())
-	matTheta.Mul(&matTheta, matY)
+	mat2.Mul(&mat1, matXv.T())
+	matTheta.Mul(&mat2, matY)
 	row, _ := matTheta.Caps()
 	for i := 0; i < row; i++ {
-		c.thetas = append(c.thetas, matTheta.At(i, 0))
+		crv.thetas = append(crv.thetas, matTheta.At(i, 0))
 	}
+	return nil
+}
+
+func (crv *Curve) Verify() error {
+	for i := 0; i < len(crv.samples); i++ {
+		var sum float64
+		for j := 0; j < crv.pow+1; j++ {
+			sum += math.Pow(crv.samples[i][0], float64(j)) * crv.thetas[j]
+		}
+		if math.Abs(sum-crv.samples[i][1]) > 0.000001 {
+			return fmt.Errorf("sample f(%v) = %v, expect %v", crv.samples[i][0], sum, crv.samples[i][1])
+		}
+	}
+
+	return nil
 }
--- src/curve/curve_test.go
+++ src/curve/curve_test.go
@@ -3,15 +3,102 @@ package curve_test
 import (
 	"fmt"
 	"gobox/src/curve"
+	"math"
+	"math/rand"
 	"testing"
+
+	"gonum.org/v1/gonum/mat"
 )
 
-func TestSort(t *testing.T) {
+func TestCurve(t *testing.T) {
 	crv := curve.NewCurve(2)
-	crv.AddSample(0.1, 1)
-	crv.AddSample(0.4, 4)
-	crv.AddSample(0.2, 2)
-	crv.AddSample(0.3, 3)
-	crv.Fitting()
+	crv.AddSample(29077, -15)
+	crv.AddSample(30657, -11)
+	crv.AddSample(35961, 30)
+	crv.AddSample(39608, 65)
+	crv.AddSample(41605, 85)
+	crv.AddSample(42572, 95)
 	fmt.Println(crv)
+	if err := crv.Fitting(); err != nil {
+		t.Fatalf(err.Error())
+	}
+	fmt.Println(crv)
+
+	if err := crv.Verify(); err != nil {
+		t.Fatalf(err.Error())
+	}
+}
+
+func TestDense(t *testing.T) {
+	a := mat.NewDense(6, 2, nil)
+	for i := 1; i < 7; i++ {
+		a.SetRow(i-1, []float64{float64(i), rand.NormFloat64()})
+	}
+	t.Logf("\na^T =\n%v", mat.Formatted(a.T()))
+	t.Logf("\na =\n%v", mat.Formatted(a))
+
+	var b, c mat.Dense
+	b.Mul(a.T(), a)
+	t.Logf("\nb = a^T * a =\n%v", mat.Formatted(&b))
+	err := b.Inverse(&b)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	t.Logf("\nb^{-1} =\n%v", mat.Formatted(&b))
+
+	c.Mul(&b, a.T())
+	t.Logf("\nc = b^{-1} * a^T\n =\n%v", mat.Formatted(&b))
+}
+
+func TestSolve(t *testing.T) {
+	x := []float64{29077, 30657, 35961, 39608, 41605, 42572}
+	y := []float64{-15, -11, 30, 65, 85, 95}
+	a := mat.NewDense(6, 3, nil)
+	for i := 0; i < 6; i++ {
+		a.SetRow(i, []float64{1, x[i], math.Pow(x[2], 2)})
+	}
+	b := mat.NewVecDense(6, y)
+	var c mat.VecDense
+	if err := c.SolveVec(a, b); err != nil {
+		t.Fatalf(err.Error())
+	}
+	t.Logf("\n%v\n", c)
+}
+
+func TestSVD(t *testing.T) {
+	s_x := []float64{29077, 30657, 35961, 39608, 41605, 42572}
+	s_y := []float64{-15, -11, 30, 65, 85, 95}
+
+	a := mat.NewDense(6, 3, nil)
+	for i := 0; i < 6; i++ {
+		a.SetRow(i, []float64{1, s_x[i], math.Pow(s_x[2], 2)})
+	}
+
+	var svd mat.SVD
+	if ok := svd.Factorize(a, mat.SVDFull); !ok {
+		t.Fatalf("failed to factorize a")
+	}
+
+	const rcond = 1e-15
+	rank := svd.Rank(rcond)
+	if rank == 0 {
+		t.Fatalf("zero rank system")
+	}
+
+	b := mat.NewVecDense(6, s_y)
+
+	var x mat.VecDense
+	svd.SolveVecTo(&x, b, rank)
+
+	t.Log(x)
+
+	for i := 0; i < 6; i++ {
+		var sum float64
+		for j := 0; j < 3; j++ {
+			sum += math.Pow(s_x[i], float64(j)) * x.At(j, 0)
+		}
+		if math.Abs(s_y[i]-sum) > 0.000001 {
+			t.Fatalf("sample f(%v) = %v, expect %v", s_x[i], sum, s_y[i])
+		}
+	}
 }
